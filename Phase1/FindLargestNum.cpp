96c120b2700ec07651cb5961b4913ca4f709fe32
Sreya-Ops <sreya289k@gmail.com>
2024-11-07 11:31:24 +0530

    Update FindLargestNum.cpp

--- Phase1/FindLargestNum.cpp
+++ Phase1/FindLargestNum.cpp
@@ -1,39 +1,21 @@
 // Finding largest number
 
 Pseudocode
-BEGIN 
+
+BEGIN
+FUNCTION Findlargest(n1,n2,n3)
+IF n1>=n2 AND n1>=n3
+        RETURN n1
+ELSE IF n2>=n1 AND n2>=n3
+        RETURN n2
+ELSE 
+        RETURN n3
+END IF
+END FUNCTION
+
+//Main function
+
 INPUT n1,n2,n3
-IF n1 >= n2 AND n1 >= n3
-        PRINT n1 is the largest
-    ELSE IF n2 >= n1 AND n2 >= n3
-        PRINT n2 is the largest
-    ELSE
-        PRINT n3 is the largest
-    END IF
+largest=Findlargest(n1,n2,n3)
+PRINT largest
 END
-
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    int n1,n2,n3;
-    
-    cout << "Enter first number: "; 
-    cin >> n1; 
-    
-    cout << "Enter second number: "; 
-    cin >> n2;
-    
-    cout << "Enter third number: "; 
-    cin >> n3; 
-
-    if (n1 >= n2 && n1 >= n3)
-        cout << n1 << "is the largest" ;
-    else if (n2 >= n1 && n2 >= n3)
-        cout << n2 << " is the largest" ;
-    else
-        cout << n3 << " is the largest" ;
-
-    return 0;
-}
