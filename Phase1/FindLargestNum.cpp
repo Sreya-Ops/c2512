7e190e3b55662f66674c0e11c6fda5a83b6bac9f
Sreya-Ops <sreya289k@gmail.com>
2024-11-07 07:41:38 +0530

    Update FindLargestNum.cpp

--- Phase1/FindLargestNum.cpp
+++ Phase1/FindLargestNum.cpp
@@ -18,9 +18,9 @@ int main() {
     if (n1 >= n2 && n1 >= n3)
         cout << n1 << "is the largest" ;
     else if (n2 >= n1 && n2 >= n3)
-        cout << n2 << "is the largest" ;
+        cout << n2 << " is the largest" ;
     else
-        cout << n3 << "is the largest" ;
+        cout << n3 << " is the largest" ;
 
     return 0;
 }
