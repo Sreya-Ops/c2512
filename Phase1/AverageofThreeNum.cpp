ecc58006a5b62373555b0df88bb821876b464719
Sreya-Ops <sreya289k@gmail.com>
2024-11-08 10:25:41 +0530

    Update AverageofThreeNum.cpp

--- Phase1/AverageofThreeNum.cpp
+++ Phase1/AverageofThreeNum.cpp
@@ -1,14 +1,14 @@
 //Find the average of three numbers
-Pseudo Function:
+Pseudo Functions:
 
-FUNCTION findAverage(number1,number2,number3)
+FUNCTION findAverage(number1->int, number2->int, number3->int)
 sum=number1+number2+number3
 average=sum/3
 RETURN average
 
 FUNCTION findAverageTest()
-INPUT number1,number2,number3
-average= findAverage(number1,number2,number3)
+INPUT number1, number2, number3
+average= findAverage(number1, number2, number3)
 PRINT average
 
 Pseudocode:
