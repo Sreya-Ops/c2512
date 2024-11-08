f6202c03585c38639db264c94a35134b9c9ce991
Sreya-Ops <sreya289k@gmail.com>
2024-11-08 10:22:55 +0530

    Update FindLargestNum.cpp

--- Phase1/FindLargestNum.cpp
+++ Phase1/FindLargestNum.cpp
@@ -3,7 +3,7 @@
 Pseudo function:
 
 BEGIN
-FUNCTION findLargest(number1,number2,number3)->int
+FUNCTION findLargest(number1->, number2->, number3->)->int
 IF number1>=number2 AND number1>=number3
         RETURN number1
 ELSE IF number2>=number1 AND number2>=number3
@@ -14,8 +14,8 @@ END IF
 END FUNCTION
 
 FUNCTION findLargestTest()
-INPUT number1,number2,number3
-int largest=findLargest(number1,number2,number3)
+INPUT number1, number2, number3
+int largest=findLargest(number1, number2, number3)
 PRINT largest
 END
 
