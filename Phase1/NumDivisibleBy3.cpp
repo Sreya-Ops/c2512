0646816cf56bc1d246f9cace3e0c8d1387da98d0
Sreya-Ops <sreya289k@gmail.com>
2024-11-08 07:59:52 +0530

    Add files via upload

---
+++ Phase1/NumDivisibleBy3.cpp
@@ -0,0 +1,47 @@
+//Number divisible by 3 or not
+
+Pseudo function:
+function isDivisibleByThree(number->)
+    if number MOD 3==0
+        return true
+    endif
+    return false
+	
+function isDivisibleByThreeTest()
+    input number
+    result=isDivisibleByThree(number)
+    if result
+        print"The number is divisible by 3"
+    else
+        print"The number is not divisible by 3"
+
+Pseudocode:
+isDivisibleByThreeTest()
+
+#include<iostream>
+using namespace std;
+
+bool isDivisibleByThree(int number){
+    if(number%3==0){
+        return true;
+    }
+    return false;
+}
+
+void isDivisibleByThreeTest(){
+    int number;
+    cout<< "Enter a Number:";
+    cin>> number;
+    int result=isDivisibleByThree(number);
+    if(result){
+        cout<< "The number is divisible by 3";
+    }
+    else{
+        cout<< "The number is not divisible by 3";
+    }
+}
+
+int main(){
+    isDivisibleByThreeTest();
+    return 0;
+}
