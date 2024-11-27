14d6beef2027a79f52446fb67110395dab072b84
Sreya-Ops <sreya289k@gmail.com>
2024-11-27 21:19:23 +0530

    Add files via upload

--- Phase1/Tasks/Task4/Patient_class/main.cpp
+++ Phase1/Tasks/Task4/Patient_class/main.cpp
@@ -8,9 +8,12 @@ int main() {
     Patients p2("P002", 50);
 
     std::cout << std::boolalpha;
-    std::cout << "Equals: " << p1.Equals(p2) << std::endl;          // Output: false
-    std::cout << "GreaterThan: " << p1.GreaterThan(p2) << std::endl; // Output: false
-    std::cout << "LessThanEquals: " << p1.LessThanEquals(p2) << std::endl; // Output: true
+    std::cout << "Equals: " << p1.Equals(p2) << std::endl;      
+    std::cout << "NotEquals: " << p1.NotEquals(p2) << std::endl;         
+    std::cout << "GreaterThan: " << p1.GreaterThan(p2) << std::endl; 
+    std::cout << "GreaterThanEquals: " << p1.GreaterThanEquals(p2) << std::endl;
+    std::cout << "LessThan: " << p1.LessThan(p2) << std::endl; 
+    std::cout << "LessThanEquals: " << p1.LessThanEquals(p2) << std::endl; 
 
     return 0;
 }
